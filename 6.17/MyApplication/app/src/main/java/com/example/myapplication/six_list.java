3e8206c53b30b668286f1324f644895b6a5be627
MacAir <macair@MacAirui-MacBook-Air.local>
2019-06-17 12:37:46 +0900

    .

---
+++ 6.17/MyApplication/app/src/main/java/com/example/myapplication/six_list.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class six_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_six_list);
+    }
+}
---
+++ 6.17/MyApplication/app/src/main/java/com/example/myapplication/three_list.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class three_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three_list);
+    }
+}
